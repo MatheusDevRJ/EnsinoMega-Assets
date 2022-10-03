6d56bda72dd150858458c153c749b706d57a543f
MatheusDevRJ <101483884+MatheusDevRJ@users.noreply.github.com>
2022-10-02 23:26:08 -0300

    Delete code-backup.js

--- code-backup.js
+++
@@ -1,6 +0,0 @@
-    lsTheme: _ => {
-      const notes = getScale("major", 50);
-      const positions = [3, 1, 4, 1, 2, 3, 1, 5, 4, 3, 2, 1, 0];
-      const songNotes = getArrayPositions(notes, positions);
-      play.notes(songNotes, 200);
-    },
